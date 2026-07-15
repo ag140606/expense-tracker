f19ea73173bec8429b47b3d9224b67edcba28594
ag140606 <anagha.g1406@gmail.com>
2026-07-15 23:38:43 +0530

    Stylesheet version updated

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ function catColor($cat, $map) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Expense Tracker</title>
-<link rel="stylesheet" href="css/style.css?v=2">
+<link rel="stylesheet" href="css/style.css?v=3">
 </head>
 <body>
 
